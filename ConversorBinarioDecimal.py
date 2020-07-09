196d917198d037be84e8c11697f45c93e2991cf1
guilhermemorningstar <guilhermemorningstar@yahoo.com>
2020-07-09 16:43:32 -0300

    Finalizado

---
+++ ConversorBinarioDecimal.py
@@ -0,0 +1,19 @@
+def converterd_b(n)
+    binario = 
+    while(True)
+        binario = binario + str(n%2)
+        n = n2
+        if n == 0
+            break
+    binario = binario[-1]
+    binario = int(binario)
+    return binario
+def converterb_d(n)
+    decimal = 0
+    n = str(n)
+    n = n[-1]
+    tam = len(n)
+    for i in range(tam)
+        if n[i] == 1
+            decimal = decimal + 2i
+    return decimal
